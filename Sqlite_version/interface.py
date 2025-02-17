9c3276ba089c8a568a33a28fb230c10c0abdd9ba
Diogo <diogo.nogueira.santos@outlook.com>
2025-02-17 13:42:27 -0300

    Atualização e correções de bugs

--- Sqlite_version/interface.py
+++ Sqlite_version/interface.py
@@ -1,10 +1,10 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QCheckBox, QPushButton, QMessageBox
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QCheckBox, QPushButton, QMessageBox, QHBoxLayout, QMainWindow
 from PyQt6.QtSql import QSqlDatabase, QSqlQuery
-from PyQt6.QtGui import QIcon, QMovie, QFont
+from PyQt6.QtGui import QIcon, QMovie, QFont, QPixmap, QAction
 from PyQt6.QtCore import QSize
 import sys
-from novo_Usuario import New_User
 
+from novo_Usuario import New_User
 
 
 class Login(QWidget):
@@ -40,9 +40,13 @@ class Login(QWidget):
         self.user_Animation.start()
         self.password_Animation.start()
         self.user_Line = QLineEdit(self)
+        self.user_Line.setClearButtonEnabled(True)
         self.password_Line = QLineEdit(self)
+        self.password_Line.setClearButtonEnabled(True)
         self.hidden_password = QCheckBox('Show Password', self)
         self.user_Botton = QPushButton('Login', self)
+        self.user_Botton.setProperty('class', 'settings_Buttons')
+        self.user_Botton.setEnabled(False)
         self.user_Label.move(100, 100)
         self.password_Label.move(97, 150)
         self.user_Line.move(130, 103)
@@ -53,15 +57,22 @@ class Login(QWidget):
         self.password_Line.setPlaceholderText('Password')
         self.password_Line.setEchoMode(QLineEdit.EchoMode.Password)
         self.user_Botton.clicked.connect(self.search_User)
-        self.hidden_password.toggled.connect(self.settings_box_lineedit)
+        self.hidden_password.toggled.connect(self.settings_hidden_check_box)
+        self.user_Line.textEdited.connect(self.settings_Enable_box_line)
 
 
-    def settings_box_lineedit(self, checked):
+
+    def settings_hidden_check_box(self, checked):
         if checked:
             self.password_Line.setEchoMode(QLineEdit.EchoMode.Normal)
         else:
             self.password_Line.setEchoMode((QLineEdit.EchoMode.Password))
 
+    def settings_Enable_box_line(self):
+        if  self.user_Line.text() != "":
+            self.user_Botton.setEnabled(True)
+        else:
+            self.user_Botton.setEnabled(False)
 
     def search_User(self):
         self.sql_cursor = QSqlQuery(self.base_Informations())
@@ -74,6 +85,7 @@ class Login(QWidget):
                 QMessageBox.information(self, "Usuário encontrado!", f"seja bem-vindo(a) {self.user_Line.text()}",
                                         QMessageBox.StandardButton.Ok)
                 self.linke = Main_Window()
+                self.close()
             else:
                 QMessageBox.question(self, "Usuário não encotrado!", """<p>Infelizmente não foi possíviel encontra o usuário!</p>
                         <p>Deseja tentar novamente?</p>""",
@@ -90,11 +102,13 @@ class Login(QWidget):
     def create_Account_Settings(self):
         self.create_User_Label = QLabel("Don't have a account?", self)
         self.create_User_Button = QPushButton('Create', self)
+        self.create_User_Button.setProperty('class', 'settings_Buttons')
         self.create_User_Label.move(30, 280)
         self.create_User_Button.move(150, 315)
         self.create_User_Button.clicked.connect(self.send_newWindow)
 
-    def send_newWindow(self):
+
+    def send_newWindow(self, event):
         self.link_Class = New_User()
         self.link_Class.show()
 
@@ -102,7 +116,8 @@ class Login(QWidget):
 
 
 
-class Main_Window(QWidget):
+
+class Main_Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.InitializationUI()
@@ -110,13 +125,36 @@ class Main_Window(QWidget):
     def InitializationUI(self):
         self.setGeometry(850, 850, 900, 900)
         self.setWindowTitle("Your finance")
+        self.menu_Bar()
         self.show()
+
+    def menu_Bar(self):
+
+        fechar_Programa = QAction('Sair', self)
+        fechar_Programa.setShortcut('Ctrl+Q')
+        fechar_Programa.setStatusTip('Encerrar programa')
+        fechar_Programa.triggered.connect(self.close)
+
+        main_Menu =  self.menuBar()
+        main_Menu.setProperty('class', 'main_menu')
+        investimentos = main_Menu.addMenu('Investimentos')
+        renda = main_Menu.addMenu('Renda')
+        extrato = main_Menu.addMenu('Extrato')
+        cartoes = main_Menu.addMenu('Cartões')
+        ajuda = main_Menu.addMenu('Ajuda')
+        sair = main_Menu.addMenu("&Sair")
+        sair.addAction(fechar_Programa)
+
+
+
 
 if __name__ in "__main__":
     app = QApplication(sys.argv)
     program = Login()
-    app.styleSheet()
-    app.exec()
+    with open('style.css', 'r') as arquivo:
+        style = arquivo.read()
+        app.setStyleSheet(style)
+    sys.exit(app.exec())
 
 
 
--- Sqlite_version/novo_Usuario.py
+++ Sqlite_version/novo_Usuario.py
@@ -1,14 +1,28 @@
-from PyQt6.QtWidgets import QApplication, QDialog, QLabel, QLineEdit, QCheckBox, QPushButton, QMessageBox
-from PyQt6.QtGui import QIcon, QMovie, QFont
+import sys
+
+from PyQt6.QtWidgets import QApplication, QDialog, QLabel, QLineEdit, QCheckBox, QPushButton, QMessageBox, QHBoxLayout
+from PyQt6.QtGui import QIcon, QMovie, QFont, QImage, QPixmap
 from PyQt6.QtSql import QSqlDatabase, QSqlQuery
 from PyQt6.QtCore import QSize
 
-
-
+style = """
+    .settings_Buttons{
+      background:#000;
+      color:#fff;
+      padding: 5px;
+      border-radius: 7px;
+    }
+    
+    .settings_Buttons:hover{
+      background: #7c7a7a;
+      color: #fff;
+    }
+"""
 
 class New_User(QDialog):
     def __init__(self):
         super().__init__()
+        self.setModal(True)
         self.InitializationUI()
 
     def InitializationUI(self):
@@ -41,6 +55,9 @@ class New_User(QDialog):
         self.createuser_Lineedit = QLineEdit(self)
         self.createpassword_Lineedit = QLineEdit(self)
         self.confirm_Userpassword_Lineedit = QLineEdit(self)
+        self.createuser_Lineedit.setClearButtonEnabled(True)
+        self.createpassword_Lineedit.setClearButtonEnabled(True)
+        self.confirm_Userpassword_Lineedit.setClearButtonEnabled(True)
         self.createuser_Lineedit.move(130, 103)
         self.createpassword_Lineedit.move(133, 150)
         self.confirm_Userpassword_Lineedit.move(133, 195)
@@ -50,14 +67,18 @@ class New_User(QDialog):
         self.createpassword_Lineedit.setEchoMode(QLineEdit.EchoMode.Password)
         self.confirm_Userpassword_Lineedit.setEchoMode(QLineEdit.EchoMode.Password)
         self.createuser_Button = QPushButton('Create', self)
+        self.createuser_Button.setStyleSheet(style)
+        self.createuser_Button.setProperty('class', 'settings_Buttons')
+        self.createuser_Button.setEnabled(False)
         self.createuser_Button.move(150, 270)
         self.hidden_Password = QCheckBox('Show password', self)
         self.hidden_Password.move(100, 230)
-        self.hidden_Password.toggled.connect(self.settings_box_lineedit)
+        self.hidden_Password.toggled.connect(self.settings_CheckBox)
         self.createuser_Button.clicked.connect(self.create_User)
+        self.createuser_Lineedit.textEdited.connect(self.settings_Button)
 
 
-    def settings_box_lineedit(self, checked):
+    def settings_CheckBox(self, checked):
         if checked:
             self.createpassword_Lineedit.setEchoMode(QLineEdit.EchoMode.Normal)
             self.confirm_Userpassword_Lineedit.setEchoMode(QLineEdit.EchoMode.Normal)
@@ -65,19 +86,26 @@ class New_User(QDialog):
             self.createpassword_Lineedit.setEchoMode(QLineEdit.EchoMode.Password)
             self.confirm_Userpassword_Lineedit.setEchoMode(QLineEdit.EchoMode.Password)
 
-
+    def settings_Button(self):
+        if self.createuser_Lineedit.text() != "":
+            self.createuser_Button.setEnabled(True)
+        else:
+            self.createuser_Button.setEnabled(False)
 
     def create_User(self):
         self.sql_Cursor = QSqlQuery(self.base_Informations())
         add_NewUser = 'insert into Users (Name, user_Password) values (?, ?)'
         self.sql_Cursor.prepare(add_NewUser)
         if self.createpassword_Lineedit.text() == self.confirm_Userpassword_Lineedit.text():
-            user_Name = self.createuser_Lineedit.text()
-            Password = self.createpassword_Lineedit.text()
+            user_Name = self.createuser_Lineedit.text().strip()
+            Password = self.createpassword_Lineedit.text().strip()
             self.sql_Cursor.addBindValue(user_Name)
             self.sql_Cursor.addBindValue(Password)
             if self.sql_Cursor.exec():
                 self.database_Connection.commit()
-                QMessageBox.information(self, 'Usuário criado com sucesso!', f'Seja muito bem vindo(a) {self.createuser_Lineedit.text()}', QMessageBox.StandardButton.Ok)
+                QMessageBox.information(self, 'Usuário criado com sucesso!', f'Seja muito bem vindo(a) {self.createuser_Lineedit.text().strip()}', QMessageBox.StandardButton.Ok)
                 self.close()
+        elif self.createpassword_Lineedit.text() != self.confirm_Userpassword_Lineedit.text():
+            QMessageBox.information(self, 'Falha ao criar o usuário!', 'Ops... parece que as senhas não se coincidem!', QMessageBox.StandardButton.Ok)
+
 
